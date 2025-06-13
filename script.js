b662be0ef8142405a2850662bf1d19115e7b5971
Fatimah Badmus <fatimahbadmus04@gmail.com>
2025-06-13 12:47:48 +0100

    Implement form validation for registration with feedback messages

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
-"use strict"
+"use strict;"
+
+document.addEventListener('DOMContentLoaded', function (){
+    const form = document.getElementById('registration-form');
+    const feedBackDiv = document.getElementById('form-feedback');
+   
+    
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const username= document.getElementById('username').value.trim();
+        const email = document.getElementById('email').value.trim(); 
+        const password = document.getElementById('password').value.trim();
+        let isValid = true;
+        const messages = [];
+
+
+        if (username.length < 3) {
+            messages.push('Username must be at least 3 characters long.');
+            isValid = false;
+        }
+        if (!email.includes('@') || !email.includes('.')) {
+            messages.push('Email must contain an "@" symbol.');
+            isValid = false;
+        }
+        if (!password.length <= 8) {
+            messages.push('Password must be at least 8 characters long.');
+            isValid = false;
+        }
+    
+            feedBackDiv.style.display = 'block';
+        if (isValid=== true) {
+            feedBackDiv.textContent = 'Registration successful!';
+            feedBackDiv.style.color = "#28a745";
+        } else {
+            feedBackDiv.innerHTML = messages.join('<br>');
+            feedBackDiv.style.color = "#dc3545";
+        }
+        
+    
+    
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
